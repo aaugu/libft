5131834e4448d752add61a40087eb3f5502e6b42
Anais Augu <aaugu@c1r11s1.42lausanne.ch>
2022-10-31 11:32:46 +0100

    ft_strrchr ok

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,10 +6,36 @@
 /*   By: aaugu <marvin@42lausanne.ch>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 10:19:34 by aaugu             #+#    #+#             */
-/*   Updated: 2022/10/27 11:57:50 by aaugu            ###   ########.fr       */
+/*   Updated: 2022/10/31 11:27:01 by aaugu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strrchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c)
+{
+	char	*str;
+	int		len;
+
+	str = (char *)s;
+	len = ft_strlen(s);
+	while (len >= 0)
+	{
+		if (str[len] == (char)c)
+			return (&str[len]);
+		len--;
+	}
+	return (0);
+}
+/*
+int	main(void)
+{
+   const char str[] = "http://www.tutorialspoint.com";
+   const char ch = '.';
+   char *ret;
+
+	ret = strchr(str, ch);
+	printf("String after |%c| is - |%s|\n", ch, ret);
+	return (0);
+}
+*/
