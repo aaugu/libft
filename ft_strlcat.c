423c9e82e5e81a72a4d3377ff0b4a2b706ba68eb
Anais Augu <aaugu@c1r11s1.42lausanne.ch>
2022-10-31 16:53:09 +0100

    ft_strlcat ok

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,31 +6,28 @@
 /*   By: aaugu <marvin@42lausanne.ch>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 12:02:33 by aaugu             #+#    #+#             */
-/*   Updated: 2022/10/31 14:35:03 by aaugu            ###   ########.fr       */
+/*   Updated: 2022/10/31 16:51:24 by aaugu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-/*
+
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	i;
-	size_t	j;
-	size_t	dlen;
+	size_t	dst_len;
+	size_t	src_len;
 
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+	if (dst_len >= dstsize)
+		return (dstsize + src_len);
 	i = 0;
-	j = 0;
-	while (dst[i])
-		i++;
-	while (src[i] && j < dstsize - 1)
+	while (src[i] && dst_len + i < dstsize - 1)
 	{
-		dst[i] = src[j];
+		dst[dst_len + i] = src[i];
 		i++;
-		j++;
 	}
-	dst[i] = '\0';
-	while (src[i])
-		i++;
-	return (i);
+	dst[dst_len + i] = '\0';
+	return (dst_len + src_len);
 }
-*/
