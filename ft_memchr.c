909033192fc2149382135fed180ea3b590f59955
Anais Augu <aaugu@c1r11s1.42lausanne.ch>
2022-10-31 11:14:39 +0100

    ft-strchr ok

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,15 +6,15 @@
 /*   By: aaugu <marvin@42lausanne.ch>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 12:02:18 by aaugu             #+#    #+#             */
-/*   Updated: 2022/10/31 10:12:36 by aaugu            ###   ########.fr       */
+/*   Updated: 2022/10/31 10:55:15 by aaugu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-/*
+
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t	i;
+	size_t			i;
 	unsigned char	*str;
 
 	i = 0;
@@ -26,5 +26,17 @@ void	*ft_memchr(const void *s, int c, size_t n)
 		if (str[i] == c)
 			return (s[i]);
 	}
+	return (0);
+}
+/*
+int	main(void)
+{
+   const char	str[] = "http://www.tutorialspoint.com";
+   const char	ch = '.';
+   char	*ret;
+
+	ret = memchr(str, ch, strlen(str));
+	printf("String after |%c| is - |%s|\n", ch, ret);
+	return (0);
 }
 */
--- ft_strchr.c
+++ ft_strchr.c
@@ -6,22 +6,38 @@
 /*   By: aaugu <marvin@42lausanne.ch>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 10:19:56 by aaugu             #+#    #+#             */
-/*   Updated: 2022/10/28 11:24:07 by aaugu            ###   ########.fr       */
+/*   Updated: 2022/10/31 11:12:48 by aaugu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-/*
+
 char	*ft_strchr(const char *s, int c)
 {
-	int	i;
+	char	*str;
+	int		i;
 
 	i = 0;
-	while (s[i])
+	str = (char *)s;
+	while (str[i])
 	{
-		if (s[i] == (char)c)
-			return (s[i]);
+		if (str[i] == (char)c)
+			return (&str[i]);
+		i++;
 	}
-	return (NULL);
+	if (str[i] == (char)c)
+		return (&str[i]);
+	return (0);
+}
+/*
+int	main(void)
+{
+   const char str[] = "http://www.tutorialspoint.com";
+   const char ch = '.';
+   char *ret;
+
+   ret = strchr(str, ch);
+   printf("String after |%c| is - |%s|\n", ch, ret);
+   return (0);
 }
 */
