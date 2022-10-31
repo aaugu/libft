3ef03390227f1bf0c43f52fbe584be73c2faec66
Anais Augu <aaugu@c1r11s1.42lausanne.ch>
2022-10-31 12:38:26 +0100

    ft_strncmp ok

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,39 +6,34 @@
 /*   By: aaugu <marvin@42lausanne.ch>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 12:03:22 by aaugu             #+#    #+#             */
-/*   Updated: 2022/10/31 10:14:04 by aaugu            ###   ########.fr       */
+/*   Updated: 2022/10/31 11:45:18 by aaugu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-/*
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
-int ft_strncmp(const char *str1, const char *str2, size_t n)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	unsigned int	i;
+	size_t	i;
 
 	i = 0;
 	if (n == 0)
 		return (0);
 	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0' && i < n - 1)
 		i++;
-	return (s1[i] - s2[i]);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
-
+/*
 int	main(void)
 {
-	char	s1[] = "Hello !";
-	char	s2[] = "Hello World";
+	char	s1[] = "\x12\xff\x65\x12\xbd\xde\xad";
+	char	s2[] = "\x12\x02";
 
-	printf("%d\n", strncmp(s1, s2, 4));
-	printf("%d\n", ft_strncmp(s1, s2, 4));
+	printf("%d\n", strncmp(s1, s2, 1));
+	printf("%d\n", ft_strncmp(s1, s2, 1));
 	printf("%s", "-----\n");
-	printf("%d\n", strncmp(s1, s2, 7));
-	printf("%d\n", ft_strncmp(s1, s2, 7));
-	printf("%s", "-----\n");
-	printf("%d\n", strncmp(s2, s1, 0));
-	printf("%d\n", ft_strncmp(s2, s1, 0));
+	printf("%d\n", strncmp(s1, s2, 6));
+	printf("%d\n", ft_strncmp(s1, s2, 6));
 	return (0);
 }
 */
