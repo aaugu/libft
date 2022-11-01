9cab60c0e4d90a83044defbcc79399f3e6a91d27
Anais Augu <aaugu@c1r11s1.42lausanne.ch>
2022-11-01 15:01:42 +0100

    ft_strjoin ok

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,10 +6,34 @@
 /*   By: aaugu <marvin@42lausanne.ch>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 13:26:39 by aaugu             #+#    #+#             */
-/*   Updated: 2022/10/27 11:57:24 by aaugu            ###   ########.fr       */
+/*   Updated: 2022/11/01 15:00:18 by aaugu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	str = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!str)
+		return (0);
+	i = 0;
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+	{
+		str[i] = s2[j];
+		i++;
+		j++;
+	}
+	str[i] = '\0';
+	return (str);
+}
--- ft_substr.c
+++ ft_substr.c
@@ -6,10 +6,27 @@
 /*   By: aaugu <marvin@42lausanne.ch>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 13:43:32 by aaugu             #+#    #+#             */
-/*   Updated: 2022/10/27 11:58:03 by aaugu            ###   ########.fr       */
+/*   Updated: 2022/11/01 14:36:50 by aaugu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+/*
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*str;
+	size_t	i;
 
-char	*ft_substr(char const *s, unsigned int start, size_t len);
+	if (ft_strlen(s) - (size_t)start > len)
+		str = malloc(sizeof(str) * (len + 1));
+	else
+		str = malloc(sizeof(str) * (ft_strlen(s) - len + 1));
+	if (!str)
+		return (0);
+	i = 0;
+	while ((size_t)start < len)
+		str[i++] = s[start++];
+	str[i] = '\0';
+	return (str);
+}
+*/
