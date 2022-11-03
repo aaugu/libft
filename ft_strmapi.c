5e8be6db308818a4a6680f7dbf9e96e97d2ddc13
Anais Augu <aaugu@c1r11s1.42lausanne.ch>
2022-11-03 11:48:08 +0100

    ft_strmapi ok

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,10 +6,25 @@
 /*   By: aaugu <marvin@42lausanne.ch>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 13:44:17 by aaugu             #+#    #+#             */
-/*   Updated: 2022/11/03 11:18:09 by aaugu            ###   ########.fr       */
+/*   Updated: 2022/11/03 11:42:39 by aaugu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	unsigned int	i;
+	char			*str;
+
+	str = ft_strdup(s);
+	if (!str)
+		return (0);
+	i = 0;
+	while (str[i])
+	{
+		str[i] = f(i, str[i]);
+		i++;
+	}
+	return (str);
+}
