b0e772d54f3913c421c04983ec15840876219256
Anais Augu <aaugu@c1r11s1.42lausanne.ch>
2022-11-03 15:19:51 +0100

    ft_substr ok

--- ft_substr.c
+++ ft_substr.c
@@ -6,27 +6,34 @@
 /*   By: aaugu <marvin@42lausanne.ch>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 13:43:32 by aaugu             #+#    #+#             */
-/*   Updated: 2022/11/01 14:36:50 by aaugu            ###   ########.fr       */
+/*   Updated: 2022/11/03 15:12:01 by aaugu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-/*
+
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*str;
 	size_t	i;
+	size_t	size;
 
-	if (ft_strlen(s) - (size_t)start > len)
-		str = malloc(sizeof(str) * (len + 1));
-	else
-		str = malloc(sizeof(str) * (ft_strlen(s) - len + 1));
+	if (!s)
+		return (0);
+	if ((size_t)start >= ft_strlen(s))
+		return (ft_strdup(""));
+	size = ft_strlen(s + start);
+	if (size < len)
+		len = size;
+	str = (char *)malloc(sizeof(char) * (len + 1));
 	if (!str)
 		return (0);
 	i = 0;
-	while ((size_t)start < len)
-		str[i++] = s[start++];
+	while (i < len)
+	{
+		str[i] = s[start + i];
+		i++;
+	}
 	str[i] = '\0';
 	return (str);
 }
-*/
