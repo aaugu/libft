1c95f378d728f6e6c51f550feff4d87d461dd531
Anais Augu <aaugu@c1r11s1.42lausanne.ch>
2022-11-04 12:00:17 +0100

    ft_lstnew ok

--- ft_lstnew_bonus.c
+++ ft_lstnew_bonus.c
@@ -6,8 +6,20 @@
 /*   By: aaugu <marvin@42lausanne.ch>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 11:28:57 by aaugu             #+#    #+#             */
-/*   Updated: 2022/11/04 11:28:58 by aaugu            ###   ########.fr       */
+/*   Updated: 2022/11/04 11:59:03 by aaugu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*lstnew;
+
+	lstnew = (t_list *)malloc(sizeof(t_list));
+	if (!lstnew)
+		return (0);
+	lstnew->content = content;
+	lstnew->next = NULL;
+	return (lstnew);
+}
