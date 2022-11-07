1ee5a824753de8e801a43f6bc7a50915e78cb894
Anais Augu <aaugu@c1r11s1.42lausanne.ch>
2022-11-07 13:24:38 +0100

    refactorised lst functions

--- ft_lstiter_bonus.c
+++ ft_lstiter_bonus.c
@@ -6,7 +6,7 @@
 /*   By: aaugu <marvin@42lausanne.ch>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 11:28:44 by aaugu             #+#    #+#             */
-/*   Updated: 2022/11/07 13:22:21 by aaugu            ###   ########.fr       */
+/*   Updated: 2022/11/07 13:24:35 by aaugu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
-	if (lst)
+	if (!lst)
 		return ;
 	while (lst->next)
 	{
