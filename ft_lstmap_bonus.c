140995a70e2dabb518885329560c1a095e789a0d
Anais Augu <aaugu@c1r11s1.42lausanne.ch>
2022-11-07 14:52:02 +0100

    ft_lstmap ok without strict

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: aaugu <marvin@42lausanne.ch>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 11:28:54 by aaugu             #+#    #+#             */
-/*   Updated: 2022/11/07 13:23:27 by aaugu            ###   ########.fr       */
+/*   Updated: 2022/11/07 14:08:07 by aaugu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,21 @@
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new;
-	t_list	*temp;
+	t_list	*new_list;
 
-	if (!lst)
+	if (!lst || !f || !del)
 		return (0);
-	while (lst->next)
+	new_list = NULL;
+	while (lst)
 	{
-		new = ft_lstnew(lst->content);
-		temp = new;
+		new = ft_lstnew(f(lst->content));
+		if (!new)
+		{
+			ft_lstclear(&new_list, del);
+			return (0);
+		}
+		ft_lstadd_back(&new_list, new);
+		lst = lst->next;
 	}
-		
+	return (new_list);
 }
