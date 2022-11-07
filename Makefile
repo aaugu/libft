8dc484feb0eb2516a068807fd521cb56a0cb0a6b
Anais Augu <aaugu@c1r11s1.42lausanne.ch>
2022-11-07 09:25:44 +0100

    ft_split ok

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: aaugu <marvin@42lausanne.ch>               +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/10/24 12:03:36 by aaugu             #+#    #+#              #
-#    Updated: 2022/11/04 15:23:26 by aaugu            ###   ########.fr        #
+#    Updated: 2022/11/07 09:24:31 by aaugu            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -19,7 +19,7 @@ ft_memchr.c ft_memcmp.c ft_strnstr.c ft_atoi.c ft_calloc.c ft_strdup.c \
 ft_substr.c ft_strjoin.c ft_strtrim.c ft_split.c ft_itoa.c ft_strmapi.c \
 ft_striteri.c ft_putchar_fd.c ft_putstr_fd.c ft_putendl_fd.c ft_putnbr_fd.c
 
-BONUS = ft_lstnew_bonus.c ft_lstadd_front_bonus.c t_lstsize_bonus.c \
+BONUS = ft_lstnew_bonus.c ft_lstadd_front_bonus.c ft_lstsize_bonus.c \
 ft_lstlast_bonus.c ft_lstadd_back_bonus.c ft_lstdelone_bonus.c \
 ft_lstclear_bonus.c ft_lstiter_bonus.c ft_lstmap_bonus.c
 
@@ -38,7 +38,7 @@ ${NAME}:	${OBJS}
 
 all: 		${NAME}
 
-bonus:		${OBJS} ${OBJS_BONUS}
+bonus:		${OBJS_BONUS}
 			${AR} ${NAME} ${OBJS_BONUS}
 
 clean:
--- ft_split.c
+++ ft_split.c
@@ -6,103 +6,69 @@
 /*   By: aaugu <marvin@42lausanne.ch>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 13:42:03 by aaugu             #+#    #+#             */
-/*   Updated: 2022/11/04 11:20:21 by aaugu            ###   ########.fr       */
+/*   Updated: 2022/11/07 09:17:00 by aaugu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-/*
-int	ft_countwords(char const *s, char c);
-int	*ft_countchars(char const *s, char c, int words);
+
+size_t	ft_countwords(char const *s, char c);
+char	**ft_freeall(char **list);
 
 char	**ft_split(char const *s, char c)
 {
-	int		i;
-	int		words;
-	int		*sizes;
+	size_t	i;
+	size_t	j;
+	size_t	save;
 	char	**strs;
 
-	words = ft_countwords(s, c);
-	strs = (char **)malloc(sizeof(char *) * (words + 1));
+	strs = (char **)malloc(sizeof(char *) * (ft_countwords(s, c) + 1));
 	if (!strs)
 		return (0);
-	strs[words] = NULL;
-	sizes = ft_countchars(s, c, words);
-
+	i = 0;
+	j = 0;
+	while (i < ft_countwords(s, c) && s[j] != '\0')
+	{
+		while (s[j] == c)
+			j++;
+		save = j;
+		while (s[j] != c && s[j] != '\0')
+			j++;
+		strs[i] = ft_substr(s, save, j - save);
+		if (strs[i++] == 0)
+			return (ft_freeall(strs));
+	}
+	strs[i] = NULL;
+	return (strs);
 }
 
-int	ft_countwords(char const *s, char c)
+size_t	ft_countwords(char const *s, char c)
 {
-	int	i;
-	int	count;
+	size_t	i;
+	size_t	count;
 
 	i = 0;
 	count = 0;
 	while (s[i])
 	{
-		if (s[i] == c && s[i + 1] != '\0')
+		if ((s[i] == c && s[i + 1] != '\0' && s[i + 1] != c) || \
+			(i == 0 && s[i] != c))
 			count++;
 		i++;
 	}
-	return (count + 1);
+	return (count);
 }
 
-int	*ft_countchars(char const *s, char c, int words)
+char	**ft_freeall(char **strs)
 {
-	int	i;
-	int	j;
-	int	count;
-	int	*sizes;
+	size_t	i;
 
-	sizes = malloc(sizeof(int) * (words + 1));
-	if (!sizes)
-		return (0);
-	sizes[words] = -1;
 	i = 0;
-	j = 0;
-	count = 0;
-	while (s[i])
+	while (strs[i])
 	{
-		if ((s[i] == c || s[i + 1] == 0) && sizes[j] != -1)
-		{
-			if (s[i + 1] == 0 && s[i] != c)
-				count++;
-			sizes[j] = count;
-			count = -1;
-			j++;
-		}
+		free(strs[i]);
 		i++;
-		count++;
 	}
-	return (sizes);
+	free(strs);
+	return (NULL);
 }
-
-int	main(void)
-{
-	char const	*s1 = "bliiiablooableeeea";
-	char const	*s2 = "bablablo";
-	char		c = 'a';
-	int			*sizes1;
-	int			*sizes2;
-	int			i;
-
-	sizes1 = ft_split(s1, c);
-	sizes2 = ft_split(s2, c);
-	i = 0;
-	while (sizes1[i] != -1)
-	{
-		printf("%d\n", sizes1[i]);
-		i++;
-	}
-	printf("-------\n");
-	i = 0;
-	while (sizes2[i] != -1)
-	{
-		printf("%d\n", sizes2[i]);
-		i++;
-	}
-	free(sizes1);
-	free(sizes2);
-	return (0);
-}
-*/
