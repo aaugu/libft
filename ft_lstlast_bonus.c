290539a6df0822bb73eb82106473d0a7ae9e7b1f
Anais Augu <aaugu@c1r11s1.42lausanne.ch>
2022-11-07 10:08:26 +0100

    ft_lstlast ok

--- ft_lstlast_bonus.c
+++ ft_lstlast_bonus.c
@@ -6,8 +6,19 @@
 /*   By: aaugu <marvin@42lausanne.ch>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 11:28:48 by aaugu             #+#    #+#             */
-/*   Updated: 2022/11/04 11:28:52 by aaugu            ###   ########.fr       */
+/*   Updated: 2022/11/07 10:04:07 by aaugu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	if (!lst)
+		return (0);
+	while (lst->next)
+	{
+		lst = lst->next;
+	}
+	return (lst);
+}
